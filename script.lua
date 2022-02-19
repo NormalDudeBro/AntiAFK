5cd532578d3d61d99c99d9466409b3764f74ffab
NormalDudeBro <68166174+NormalDudeBro@users.noreply.github.com>
2022-02-18 22:27:21 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,4 @@
-wait(3)
-local VirtualUser=game:service'VirtualUser'
-game:service('Players').LocalPlayer.Idled:connect(function()
-VirtualUser:CaptureController()
-VirtualUser:ClickButton2(Vector2.new())
-end)
---i didnt make this but you can find this through one google search of "anti afk script v3rm"
+for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+v:Disable()
+end
+--i didnt make this, credit to this person: https://v3rmillion.net/showthread.php?tid=1090538
